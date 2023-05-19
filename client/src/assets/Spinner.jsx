32e35c6a913cb860c018e6eac986f7bba862c501
monson2002 <monsonreji2002@gmail.com>
2023-05-19 23:44:52 +0530

    19-5-23 11:14pm Added Loader

---
+++ client/src/assets/Spinner.jsx
@@ -0,0 +1,11 @@
+import { Audio } from 'react-loader-spinner';
+<Audio
+  height="80"
+  width="80"
+  radius="9"
+  color="green"
+  ariaLabel="loading"
+  wrapperStyle
+  wrapperClass
+/>
+export default Audio;
